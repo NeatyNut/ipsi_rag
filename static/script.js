618c4591b54c36e2c0f815ca959b8e7e6b2faefc
Mingee KIm <89675001+NeatyNut@users.noreply.github.com>
2025-02-11 12:24:49 +0000

    fix : change fetch body form for gemini api

--- static/script.js
+++ static/script.js
@@ -13,22 +13,27 @@ function appendMessage(text, sender) {
 function sendMessage() {
     var message = userInput.value;
     var apikey = apikeyInput.value;
-    console.log(apikey);
     
     if (message === "") return;
 
     appendMessage(message, "user");
     userInput.value = "";
     
-    fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?${apikey}`, {
+    fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apikey}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ message })
+        body: JSON.stringify({'contents':[{'parts' : [{'text' : message}]}]})
     })
     .then(response => response.json())
     .then(data => {
-        if (data.reply) {
-            appendMessage(data.reply, "bot");
+        // 일단 data를 통해 컨텐츠가 해당 위치를 통해 들어오는 것을 확인했으나
+        // https://learn.microsoft.com/en-us/microsoft-edge/web-platform/site-impacting-changes 사이트를 확인을 해서 처리할 것
+
+        var return_message = data.candidates[0].contents.parts[0].text;
+        
+
+        if (return_message) {
+            appendMessage(return_message, "bot");
         } else {
             appendMessage("오류가 발생했습니다.", "bot");
         }
